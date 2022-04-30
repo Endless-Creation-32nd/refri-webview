ec411d3add43c12a2c838d35b917cc940c0c07b7
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-04-30 11:30:33 +0900

    Feat: add global style #1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 import { StyledThemeProvider } from '@definitions/styled-components';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
+import GlobalStyle from '../src/styles/globalStyles.js';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
   const queryClient = new QueryClient();
   return (
     <StyledThemeProvider>
+      <GlobalStyle />
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
@@ -15,6 +17,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       </QueryClientProvider>
     </StyledThemeProvider>
   );
-}
+};
 
-export default MyApp;
+export default WrappedApp;
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -6,11 +6,32 @@ const GlobalStyle = createGlobalStyle`
   *::before {
     box-sizing: border-box;
   }
+
+  p, h1, h2, h3, h4, h5 {
+    margin: 0;
+  }
   
+  html {
+    width: 100%;
+    height: 100%;
+  }
+
   body {
+    width: 100%;
+    height: 100%;
     margin: 0;
     padding: 0;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-family: medium, -apple-system, sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #EAEAEA;
+  }
+
+  #__next {
+    width: 100%;
+    height: 100%;
+    background: white;
   }
 `;
 
